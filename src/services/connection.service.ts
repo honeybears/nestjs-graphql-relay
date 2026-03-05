3fe365de0a9b1ddcea39256074e385da239b8f6f
강지훈 <clone0922@gmail.com>
2026-03-05 20:57:22 +0900

    fix(connection): remove connection feature

--- src/services/connection.service.ts
+++
@@ -1,5 +0,0 @@
-import { ConnectionArgs, ConnectionType } from 'src/interfaces/edge.interface';
-
-export abstract class ConnectionService {
-  abstract execute<T>(args: ConnectionArgs): Promise<ConnectionType<T>>;
-}
