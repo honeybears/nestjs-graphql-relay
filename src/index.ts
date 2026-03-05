d935e8d17c21119630449cdcbf6e90dd996652c4
강지훈 <clone0922@gmail.com>
2026-03-05 21:10:10 +0900

    fix: update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,12 @@ export { GraphQLRelayModule } from './graphql-relay.module';
 export type { GraphQLRelayModuleOptions } from './graphql-relay.module';
 
 export { NodeLoader } from './decorators/node-loader.decorator';
-export type { NodeLoaderMetadata } from './decorators/node-loader.decorator';
+export type { NodeLoaderTargetType } from './decorators/node-loader.decorator';
 
 export { NodeInterface } from './interfaces/node.interface';
-export {
-  Edge,
-  Connection,
-  ConnectionArgs,
-  PageInfo,
-} from './interfaces/edge.interface';
-export type { EdgeType, ConnectionType } from './interfaces/edge.interface';
 
 export {
   DefaultGlobalIdStrategy,
   InvalidGlobalIdException,
 } from './services/global-id.strategy';
 export type { GlobalId, GlobalIdStrategy } from './services/global-id.strategy';
-
-export { ConnectionService } from './services/connection.service';
